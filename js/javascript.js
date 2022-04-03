aec33aa457849ee8effa7bd7e866622971665c32
gaa101 <59945096+gaa101@users.noreply.github.com>
2022-04-03 13:11:06 -0400

    Assignment #3

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+//- Change the color of an element using .style
+//accordion-button.style.color = red
+
+//- Change the font size of an element using .setProperty
+
+
+//- Add a class to an HTML tag with .setAttribute, and change some property with that class.
+
+
+//- Create an empty paragraph and add some content with Javascript.
+
+
+//- Create an array and display the content on the page.
+
+
+//- Create a button and add an event listener to change the background of a section or div.
+
+
+//- Select one of the 3 elements implemented in class and integrate it into your page. (Slide Panel, Modal, or Random background). If you use the random background you can apply it to any section. It does not have to be the whole background.
