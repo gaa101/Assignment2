6509fff2cab59669aa55011794e6770dced4794e
gaa101 <59945096+gaa101@users.noreply.github.com>
2022-04-06 16:47:13 -0400

    map

--- js/javascript.js
+++ js/javascript.js
@@ -150,8 +150,13 @@ var options = {
 // ==================== Leaflet maps ==================== 
 var map = L.map('map').setView([43.7193868, 2.7938211], 17);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
 }).addTo(map);
 
 L.marker([43.7193868, 2.7938211]).addTo(map)
